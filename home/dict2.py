7447ece6b24d4c6c7970762c9b6eb49d502c3b8f
NepalRock961 <sagararyal96@gmail.com>
2017-03-25 23:21:55 +0545

    example of dictionary

---
+++ home/dict2.py
@@ -0,0 +1,13 @@
+data = [
+{
+	'name' : 'Sagar Aryal',
+	'email' : 'sagararyal96@gmail.com'
+},
+{
+	'name' : 'Dhruba Thapa',
+	'email' : 'dhrubathapa@gmail.com'
+}
+]
+
+print('Names: %s' % data[0]['name'])
+print('Email: %s' % data[0]['email'])
