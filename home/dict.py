565a198513302dff8c1973918b323b2f35354a5a
NepalRock961 <sagararyal96@gmail.com>
2017-03-25 22:59:56 +0545

    dict example

---
+++ home/dict.py
@@ -0,0 +1,10 @@
+user_info = {
+	'name' : 'sagar',
+	'email' : 'sagararyal96@gmail.com',
+	'adress' : 'kathmandu'
+
+}
+
+print('Name: %s' % user_info['name'])
+print('Email : %s' % user_info['email'])
+print('Adress : %s' % user_info['adress'])
