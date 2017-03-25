fc48fc696592b0bfcb8836832188fb230661ec46
NepalRock961 <sagararyal96@gmail.com>
2017-03-25 21:33:30 +0545

    example

---
+++ home/ex2.py
@@ -0,0 +1,7 @@
+n = [1,2,3,4,5,6,7,8,9,10]
+even_n = [x for x in range(20) if x%2 == 0]
+odd_n = [x for x in range(20) if x%2 !=0 ]
+
+print('Numebers:\n ', n)
+print('Even Numbers:\n ',even_n)
+print('Odd numbers:\n ', odd_n)
